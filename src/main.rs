a76cf04bb095da89393c5d64ca5a9bc247e1ba90
anant-357 <anantsh.88@gmail.com>
2023-10-16 16:44:46 +0530

    switched from tide to actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, HttpServer, App, Responder, HttpResponse};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Successful")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(| | {
+        App::new().service(home)
+    }).bind(("127.0.0.1", 8080))?.run().await
 }
