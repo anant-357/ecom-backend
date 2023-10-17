56b9172bc15150a455fcfcda7284dd69a894e296
anant-357 <anantsh.88@gmail.com>
2023-10-18 00:00:50 +0530

    added CORS

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use actix_web::{web, middleware::Logger, HttpServer, App};
+use actix_web::{web, middleware::Logger, HttpServer, App, http};
+use actix_cors::Cors;
 use db::Db;
 use env_logger::Env;
 mod models;
 mod db;
 mod routes;
-use crate::routes::auth;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
@@ -14,9 +14,16 @@ async fn main() -> std::io::Result<()>{
     let db: db::Db = Db::with_uri(uri.as_str()).await.unwrap();
 
     HttpServer::new(move || {
-        App::new().wrap(Logger::default())
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Cors::default()
+                  .send_wildcard()
+                  .allowed_methods(vec!["GET", "POST", "DELETE"])
+                  .allowed_headers(vec![http::header::CONTENT_TYPE]))
             .app_data(web::Data::new(db.get_db().clone()))
             .app_data(web::JsonConfig::default().limit(4096))
-            .service(auth::auth())
+            .service(routes::auth::auth())
+            .service(routes::product::product_api())
     }).bind(("127.0.0.1", 8080))?.run().await
 }
+
